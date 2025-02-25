e6c3a854235c9764c787277fa19088509c176ae1
JimMoen <LnJimMoen@outlook.com>
2025-02-25 15:54:25 +0800

    fix: improve record faces

--- erlang-ts.el
+++ erlang-ts.el
@@ -158,12 +158,10 @@ FUNC with ARGS will be called if `erlang-ts-mode' is not active."
            (:pred erlang-ts-in-type-context-p @font-lock-type-face))
      (type_name name: (atom) @font-lock-type-face)
      (export_type_attribute types: (fa fun: (atom) @font-lock-type-face))
-     (record_decl name: (atom) @font-lock-type-face
-                  (record_field name: (atom) @font-lock-property-name-face))
-     ;; for records without fields e.g
-     ;; `-record(name, {}).`
      (record_decl name: (atom) @font-lock-type-face)
-     (record_name name: (atom) @font-lock-type-face))
+     (record_name name: (atom) @font-lock-type-face)
+     (record_field name: (atom) @font-lock-constant-face)
+     (record_field_name name: (atom) @font-lock-constant-face))
 
    :language 'erlang
    :feature 'definition
