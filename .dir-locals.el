8fb305a770bc10a3015843a1fb0c80167d8767d5
Bozhidar Batsov <bozhidar@toptal.com>
2026-03-14 15:58:21 +0100

    Add .gitignore and .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil
+  (bug-reference-url-format . "https://github.com/erlang/emacs-erlang-ts/issues/%s")
+  (indent-tabs-mode . nil)
+  (fill-column . 80)
+  (checkdoc-arguments-in-order-flag))
+ (emacs-lisp-mode
+  (bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")
+  (package-lint-main-file . "erlang-ts.el")))
